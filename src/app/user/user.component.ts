32aaf2630a4ad5b1c7b8b12b5da3bd8df48ddb3d
Rishav83523 <rishav.raj@calsoftinc.com>
2024-11-30 12:21:04 +0530

    2 way data binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,7 +9,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  intialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '1';
   onSubmit() {
     console.log('Form submitted!');
+    console.log('Initial Investment: ' + this.intialInvestment);
+    console.log('Annual Investment: ' + this.annualInvestment);
+    console.log('Expected Return: ' + this.expectedReturn);
+    console.log('Duration: ' + this.duration);
   }
 }
